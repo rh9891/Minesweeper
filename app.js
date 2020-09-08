1ed8994624bb186df1394f47fd7d27b321c27bcd
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-07 23:07:11 -0400

    Randomized the board display so that the user is able to play a different game of Minesweeper each time they play. Finished creating the logic to the board before adding styling.

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector(".grid");
     let width = 10;
+    let bombAmount = 20;
     let squares = [];
 
     // Creates board for Minesweeper game.
     function createBoard() {
+        // Randomly places bombs in the grid. Gets the shuffled game array.
+        const bombsArray = Array(bombAmount).fill("bomb");
+        const emptyArray = Array(width * width - bombAmount).fill("valid");
+        const gameArray = emptyArray.concat(bombsArray);
+        console.log(gameArray);
+        const shuffledArray = gameArray.sort(() => Math.random() -0.5);
+
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement("div");
             square.setAttribute("id", i);
+            square.classList.add(shuffledArray[i]);
             grid.appendChild(square);
             squares.push(square);
         }
