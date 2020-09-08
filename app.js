3f10e54c92b06cc89d10c7d58e6653ba5f865f0a
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-08 12:18:54 -0400

    Adding numbers to the board to indicate the positions of the bombs through the use of contains(). Made initial functionality when clicking on the square to alert to Game Over if bomb is clicked.

--- app.js
+++ app.js
@@ -19,7 +19,39 @@ document.addEventListener("DOMContentLoaded", () => {
             square.classList.add(shuffledArray[i]);
             grid.appendChild(square);
             squares.push(square);
+
+            // Clicking on board.
+            square.addEventListener("click", function(event) {
+                click(square);
+            });
         }
+
+        // Displays numbers to indicate bombs for the neighboring eight squares.
+        for (let i = 0; i < squares.length; i++) {
+            let total = 0;
+            const isLeftEdge = (i % width === 0);
+            const isRightEdge = (i === width -1);
+
+            if (squares[i].classList.contains("valid")) {
+                if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb")) total++;
+                if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains("bomb")) total++;
+                if (i > 10 && squares[i - width].classList.contains("bomb")) total++;
+                if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains("bomb")) total++;
+                if (i < 98 && !isRightEdge && squares[i + 1].classList.contains("bomb")) total++;
+                if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains("bomb")) total++;
+                if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains("bomb")) total++;
+                if (i < 89 && squares[i +width].classList.contains("bomb")) total++;
+                squares[i].setAttribute("data", total);
+            }
+    }
     }
     createBoard();
+
+    // Defining the parameters of the "click" function.
+    function click(square) {
+        if (square.classList.contains("bomb")) {
+            alert("Game Over!");
+        };
+    }
+
 });
